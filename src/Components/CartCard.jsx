4d34224b3cde83aea7c7c3da4d237046d33f773b
LeoAlexThomas <leoalex960@gmail.com>
2025-11-03 18:28:43 +0530

    altered cart card style

--- src/Components/CartCard.jsx
+++ src/Components/CartCard.jsx
@@ -10,7 +10,7 @@ const CartCard = ({
   onRemoveItemFromCart,
 }) => {
   return (
-    <div className="flex flex-col md:flex-row gap-4 rounded-lg overflow-hidden border-2 border-[#F2F4F7] items-stretch pr-0 md:pr-4">
+    <div className="flex flex-col md:flex-row gap-4 rounded-lg overflow-hidden border-2 border-[#F2F4F7] items-stretch justify-between pr-0 md:pr-4">
       <div className=" p-4 h-full flex justify-center items-center w-full max-w-full md:max-w-[200px]">
         <img
           src={item.image}
@@ -18,8 +18,8 @@ const CartCard = ({
           className="w-40 h-40 rounded-lg object-contain "
         />
       </div>
-      <div className="flex grow flex-col xl:flex-row gap-4 py-4">
-        <div className="flex grow flex-col px-4 md:px-0 gap-2 ">
+      <div className="flex grow flex-col xl:flex-row gap-18 py-4">
+        <div className="flex grow flex-col px-4 md:px-0 gap-2">
           <h5 className="text-xl font-bold text-[#141519]">{item.title}</h5>
           <p className="text-base text-[#878787] line-clamp-4">
             {item.description}
@@ -28,7 +28,7 @@ const CartCard = ({
             Price: ${(item.price * itemCount).toFixed(2)}
           </p>
         </div>
-        <div className="flex flex-row xl:flex-col gap-4 items-end justify-between px-4 md:px-0 h-full">
+        <div className="flex flex-row xl:flex-col gap-4 items-center justify-center px-4 md:px-0 h-full">
           <div className="flex gap-4 border border-[#FF4F18] rounded-md items-center justify-between py-1 px-4">
             <IoIosAdd
               className="w-6 h-6 text-[#FF4F18] cursor-pointer"
